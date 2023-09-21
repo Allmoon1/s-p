44fa3b8207ab53e44dfe81713c46b3d5cc76ede1
Allmoon <clarthetrue@gmail.com>
2023-09-21 12:25:03 +0300

    File management work

--- task_01.py
+++ task_01.py
@@ -2,8 +2,8 @@ def is_palindrome(string):
     reworked_string = ''.join(char for char in str(string) if char.isalnum()).lower()
     return reworked_string == reworked_string[::-1]
 
-print(is_palindrome("A man, a plan, a canal -- Panama")) # => True
-print(is_palindrome("Madam, I'm Adam!")) # => True
-print(is_palindrome(333)) # => True
-print(is_palindrome(None)) # => False
-print(is_palindrome("Abracadabra")) # => False
+#print(is_palindrome("A man, a plan, a canal -- Panama")) # => True
+#print(is_palindrome("Madam, I'm Adam!")) # => True
+#print(is_palindrome(333)) # => True
+#print(is_palindrome(None)) # => False
+#print(is_palindrome("Abracadabra")) # => False
--- task_02.py
+++ task_02.py
@@ -1,7 +1,3 @@
-#class range(object):
-#    def __init__(self, a, b):
-#        self.coord = (a, b)
-
 def coincidence(list = [], range = None):
      result = []
      if (len(list) == 0) or (range is None):
@@ -12,8 +8,7 @@ def coincidence(list = [], range = None):
                  if (element >= range[0]) and (element <= range[-1]):
                      result.append(element)
      return result
-     
-
-print(coincidence([1, 2, 3, 4, 5], range(3, 6))) # => [3, 4, 5]
-print(coincidence()) # => []
-print(coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4))) # => [1, 2, 2.5]
+ 
+#print(coincidence([1, 2, 3, 4, 5], range(3, 6))) # => [3, 4, 5]
+#print(coincidence()) # => []
+#print(coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4))) # => [1, 2, 2.5]
