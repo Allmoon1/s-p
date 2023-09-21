80f8ed16afeaad7201a1735476cbd0131ea80f5c
Allmoon <clarthetrue@gmail.com>
2023-09-21 13:19:29 +0300

    Task4 completed

--- task_03.py
+++ task_03.py
@@ -9,8 +9,8 @@ def max_odd(array):
     else:
         return None
 
-print(max_odd([1, 2, 3, 4, 4])) # => 3
-print(max_odd([21.0, 2, 3, 4, 4])) # => 21
-print(max_odd(['ololo', 2, 3, 4, [1, 2], None])) # => 3
-print(max_odd(['ololo', 'fufufu'])) # => None
-print(max_odd([2, 2, 4])) # => None)
+#print(max_odd([1, 2, 3, 4, 4])) # => 3
+#print(max_odd([21.0, 2, 3, 4, 4])) # => 21
+#print(max_odd(['ololo', 2, 3, 4, [1, 2], None])) # => 3
+#print(max_odd(['ololo', 'fufufu'])) # => None
+#print(max_odd([2, 2, 4])) # => None)
